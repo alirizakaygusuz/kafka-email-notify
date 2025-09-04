7e760d3b9c448bd7a5e523e54bc28516b6e312f8
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2025-09-04 18:21:49 +0300

    chore: initial commit

---
+++ src/main/java/com/alirizakaygusuz/kafka_email_notify/KafkaEmailNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.alirizakaygusuz.kafka_email_notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEmailNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaEmailNotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alirizakaygusuz/kafka_email_notify/KafkaEmailNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alirizakaygusuz.kafka_email_notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEmailNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
